5ff92a0186171132791311b5766c91d4002dfb21
cwhitti <twiggleicious@gmail.com>
2024-12-04 22:41:57 +0000

    basic parrot bot functionality

--- bot.py
+++ bot.py
@@ -9,30 +9,34 @@ def run_discord_bot():
     client = discord.Client( intents=intents )
 
     # Set prefix
-    prefix = '!'
+    prefix = '.'
+    cmd_str = "say"
 
     # Show bot logged on successfully
     @client.event
     async def on_ready():
 
-        await client.change_presence(activity=discord.Game(name="New Bot!"))
+        await client.change_presence(activity=discord.Game(name="Try .say"))
         print(f'{client.user} is now running!')
 
     # Message Handler
     @client.event
     async def on_message(msg):
 
+        # Don't listen to self
+        if msg.author == client.user or msg.attachments:
+            return 0 # skip
+
         # Grab the Message Content
         content = msg.content
 
         # Check if the message starts with a known command
-        if content.startswith(prefix + "hello"):
+        if content.startswith(prefix + cmd_str):
 
-            # Grab the author's name
-            author = msg.author.name
+            to_say = content.strip( f"{prefix + cmd_str}")
 
             # Send hello back
-            await msg.channel.send(f"Hello, {author}!")
+            await msg.channel.send(f"{to_say}")
 
     # Run Bot with Token
         # Should be  the very last command inside of run_discord_bot 
